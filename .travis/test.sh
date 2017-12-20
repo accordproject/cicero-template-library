a263aeb61775262a30f248c07c0ffa53ff122419
Daniel Selman <danscode@selman.org>
2017-12-20 00:53:37 +0000

    (fix) correct test runner script

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,4 +1,12 @@
 # Exit on first error, print all commands.
 set -ev
 date
-for d in ./*/ ; do (cd "$d" && npm install && npm test); done
+
+for template in $(find ./* -maxdepth 0 -type d );
+do
+  echo "$template"
+  cd "$template"
+  npm install
+  npm test
+  cd ..
+done
