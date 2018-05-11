3a89ac24b6e6e063946930d84c465de7f31e08aa
Jerome Simeon <jeromesimeon@me.com>
2018-05-11 09:56:17 -0400

    (template) Add logic to supplychain agreement with obligations
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- supplyagreement/test/logic.js
+++ supplyagreement/test/logic.js
@@ -53,7 +53,7 @@ describe('Logic', () => {
             state.$class = 'org.accordproject.contract.State';
             const result = await engine.execute(clause, request, state, false);
             result.should.not.be.null;
-            result.response.requiredPurchase.should.equal(1020.0);
+            result.state.obligations[0].requiredPurchase.should.equal(1020.0);
         });
     });
 });
