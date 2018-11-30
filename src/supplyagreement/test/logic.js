1d421467defc9898b0e4f3231bd619e17bd56050
Jerome Simeon <jeromesimeon@me.com>
2018-11-30 10:56:56 -0500

    (template) Ergo version of supply agreement contract template
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- src/supplyagreement/test/logic.js
+++ src/supplyagreement/test/logic.js
@@ -30,7 +30,7 @@ describe('Logic', () => {
 
     const rootDir = path.resolve(__dirname, '..');
     const clauseText = fs.readFileSync(path.resolve(rootDir, 'sample.txt'), 'utf8');
-    
+
     let template;
     let clause;
     let engine;    
@@ -44,7 +44,7 @@ describe('Logic', () => {
     
     describe('#SupplyAgreement', async function() {
 
-        it('should execute a smart clause', async function () {
+        it('should execute a demand forecast', async function () {
             const request = {
                 "$class": "org.accordproject.supplyagreement.ForecastRequest",
                 "supplyForecast": 1200.0
@@ -52,10 +52,105 @@ describe('Logic', () => {
             const state = {};
             state.$class = 'org.accordproject.supplyagreement.AgreementState';
             state.stateId = 'org.accordproject.supplyagreement.AgreementState#1';
-            state.obligations = [];
+            state.purchaseObligation = null;
+            state.deliveryObligation = null;
+            state.paymentObligation = null;
             const result = await engine.execute(clause, request, state);
             result.should.not.be.null;
-            result.state.obligations[0].requiredPurchase.should.equal(1020.0);
+            result.state.purchaseObligation.requiredPurchase.should.equal(1020.0);
+            result.state.purchaseObligation.party.should.equal("PETER");
+        });
+
+        it('should execute a purchase order', async function () {
+            const request = {
+                "$class": "org.accordproject.supplyagreement.PurchaseRequest",
+                "purchaseOrder": {
+                    "$class": "org.accordproject.purchaseorder.PurchaseOrder",
+                    "products": [
+                        {
+                            "$class": "org.accordproject.purchaseorder.Product",
+                            "partNumber": "PN-001",
+                            "name": "Pink Umbrella",
+                            "quantity": 180,
+                            "unitPrice": 5.99
+                        },
+                        {
+                            "$class": "org.accordproject.purchaseorder.Product",
+                            "partNumber": "PN-002",
+                            "name": "Blue Umbrella",
+                            "quantity": 100,
+                            "unitPrice": 4.99
+                        }
+                    ],
+                    "deliveryDate": "2018/05/10",
+                    "deliveryLocation": "555 Main Street"
+                }
+            };
+            const state = {};
+            state.$class = 'org.accordproject.supplyagreement.AgreementState';
+            state.stateId = 'org.accordproject.supplyagreement.AgreementState#1';
+            state.purchaseObligation = {
+                "$class": "org.accordproject.supplyagreement.PurchaseObligation",
+                "requiredPurchase": 1020,
+                "year": 2018,
+                "quarter": 4,
+                "party": "PETER"
+            };
+            state.deliveryObligation = null;
+            state.paymentObligation = null;
+            const result = await engine.execute(clause, request, state);
+            result.should.not.be.null;
+            result.state.deliveryObligation.deliverables[0].partNumber.should.equal("Pink Umbrella");
+            result.state.deliveryObligation.party.should.equal("DAN");
+        });
+
+        it('should execute a delivery', async function () {
+            const request = {
+                "$class": "org.accordproject.supplyagreement.DeliveryRequest",
+                "products": [
+                    {
+                        "$class": "org.accordproject.purchaseorder.Product",
+                        "partNumber": "PN-001",
+                        "name": "Pink Umbrella",
+                        "quantity": 180,
+                        "unitPrice": 5.99
+                    },
+                    {
+                        "$class": "org.accordproject.purchaseorder.Product",
+                        "partNumber": "PN-002",
+                        "name": "Blue Umbrella",
+                        "quantity": 100,
+                        "unitPrice": 4.99
+                    }
+                ]
+            };
+
+            const state = {};
+            state.$class = 'org.accordproject.supplyagreement.AgreementState';
+            state.stateId = 'org.accordproject.supplyagreement.AgreementState#1';
+            state.purchaseObligation = null;
+            state.deliveryObligation = {
+                "$class": "org.accordproject.supplyagreement.DeliveryObligation",
+                "expectedDelivery": "2018-05-10T04:00:00.000Z",
+                "deliverables": [
+                    {
+                        "$class": "org.accordproject.purchaseorder.OrderItem",
+                        "partNumber": "Pink Umbrella",
+                        "quantity": 180
+                    }, 
+                    {
+                        "$class": "org.accordproject.purchaseorder.OrderItem",
+                        "partNumber": "Blue Umbrella",
+                        "quantity": 100
+                    }
+                ],
+                "party": "DAN"
+            };
+            state.paymentObligation = null;
+            const result = await engine.execute(clause, request, state);
+            result.should.not.be.null;
+            result.state.paymentObligation.amount.should.equal(1577.2);
+            result.state.paymentObligation.party.should.equal("PETER");
         });
     });
 });
