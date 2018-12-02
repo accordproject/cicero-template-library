49267464bb4af8a786d1259b6a3456a18f59bee7
Dan Selman <danscode@selman.org>
2018-12-02 14:48:02 +0000

    (feat) counter reduced based on payment amount
    Signed-off-by: Dan Selman <danscode@selman.org>

--- src/payment-upon-iot/test/logic.js
+++ src/payment-upon-iot/test/logic.js
@@ -21,12 +21,11 @@ const Engine = require('@accordproject/cicero-engine').Engine;
 const fs = require('fs');
 const path = require('path');
 const chai = require('chai');
-chai.should();
 chai.use(require('chai-things'));
 chai.use(require('chai-as-promised'));
 const moment = require('moment');
 
-describe.only('Logic', () => {
+describe('Logic', () => {
 
     const rootDir = path.resolve(__dirname, '..');
     const clauseText = fs.readFileSync(path.resolve(rootDir, 'sample.txt'), 'utf8');
@@ -134,7 +133,7 @@ describe.only('Logic', () => {
 
             const result2 = await engine.execute(clause, request2, result.state);
             result2.should.not.be.null;
-            result2.response.counter.should.equal(0);
+            result2.response.counter.should.equal(1);
             result2.response.paymentCount.should.equal(0);
             result2.state.status.should.equal("RUNNING")
             result2.emit[0].$class.should.equal('org.accordproject.cicero.runtime.PaymentObligation');
@@ -145,7 +144,11 @@ describe.only('Logic', () => {
         it('should log received payments', async function() {
 
             const request = {};
-            request.$class = 'org.accordproject.payment.PaymentReceived';
+            request.$class = 'org.accordproject.payment.iot.MonetaryAmountPayment';
+            request.amount = {
+                doubleValue: 10.0,
+                currencyCode: "USD"
+            }
             state.status = 'RUNNING';
             
             const result = await engine.execute(clause, request, state);
@@ -158,7 +161,11 @@ describe.only('Logic', () => {
         it('should complete when the maximum payments received', async function() {
 
             const request = {};
-            request.$class = 'org.accordproject.payment.PaymentReceived';
+            request.$class = 'org.accordproject.payment.iot.MonetaryAmountPayment';
+            request.amount = {
+                doubleValue: 10.0,
+                currencyCode: "USD"
+            }
             state.status = 'RUNNING';
             state.paymentCount = 4;
             
@@ -168,6 +175,61 @@ describe.only('Logic', () => {
             result.response.paymentCount.should.equal(5);
             result.state.paymentCount.should.equal(5);
             result.state.status.should.equal('COMPLETED');
+        });
+
+        it('should reduce counter by payment amount', async function() {
+
+            const request = {};
+            request.$class = 'org.accordproject.payment.iot.MonetaryAmountPayment';
+            request.amount = {
+                doubleValue: 20.0,
+                currencyCode: "USD"
+            }
+            state.status = 'RUNNING';
+            state.paymentCount = 0;
+            state.counter = 4;
+            
+            const result = await engine.execute(clause, request, state);
+            result.should.not.be.null;
+            result.response.counter.should.equal(2);
+            result.state.counter.should.equal(2);
+            result.response.paymentCount.should.equal(1);
+            result.state.paymentCount.should.equal(1);
+            result.state.status.should.equal('RUNNING');
+        });
+
+        it('counter should never be negative', async function() {
+
+            const request = {};
+            request.$class = 'org.accordproject.payment.iot.MonetaryAmountPayment';
+            request.amount = {
+                doubleValue: 1000.0,
+                currencyCode: "USD"
+            }
+            state.status = 'RUNNING';
+            state.paymentCount = 0;
+            state.counter = 4;
+            
+            const result = await engine.execute(clause, request, state);
+            result.should.not.be.null;
+            result.response.counter.should.equal(0);
+            result.response.paymentCount.should.equal(1);
+            result.state.paymentCount.should.equal(1);
+            result.state.status.should.equal('RUNNING');
+        });
+
+        it('should not be able to pay in a different currency', async function() {
+
+            const request = {};
+            request.$class = 'org.accordproject.payment.iot.MonetaryAmountPayment';
+            request.amount = {
+                doubleValue: 1000.0,
+                currencyCode: "EUR"
+            }
+            state.status = 'RUNNING';
+            state.paymentCount = 0;
+            state.counter = 4;            
+            engine.execute(clause, request, state).should.be.rejected;
         });
     });
 });
